87d63a6a6a9dbe2820f9b371e75eaa0478cbe05b
John Goerzen <jgoerzen@complete.org>
2006-12-07 02:51:17 +0100

    Removed tests

--- testsrc/Tests.hs
+++ testsrc/Tests.hs
@@ -18,50 +18,9 @@ Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 
 module Tests(tests) where
 import Test.HUnit
-import qualified MIMETypestest
-import qualified Listtest
-import qualified Maptest
-import qualified AnyDBMtest
-import qualified Pathtest
-import qualified Strtest
-import qualified IOtest
-import qualified Bitstest
-import qualified Eithertest
-import qualified ConfigParser.Parsertest
-import qualified ConfigParser.Maintest
-import qualified CRC32POSIXtest
-import qualified CRC32GZIPtest
-import qualified GZiptest
-import qualified HVIOtest
-import qualified HVFStest
-import qualified Timetest
-import qualified Str.CSVtest
-import qualified WildMatchtest
-import qualified Globtest
-import qualified ProgressTrackertest
 
 test1 = TestCase ("x" @=? "x")
 
-tests = TestList [TestLabel "test1" test1,
-                 TestLabel "List" Listtest.tests,
-                 TestLabel "Str" Strtest.tests,
-                 TestLabel "CSV" Str.CSVtest.tests,
-                 TestLabel "Time" Timetest.tests,
-                 TestLabel "Map" Maptest.tests,
-                 TestLabel "ProgressTracker" ProgressTrackertest.tests,
-                 TestLabel "AnyDBM" AnyDBMtest.tests,
-                 TestLabel "Path" Pathtest.tests,
-                 TestLabel "WildMatch" WildMatchtest.tests,
-                 TestLabel "HVIO" HVIOtest.tests,
-                 TestLabel "HVFS" HVFStest.tests,
-                 TestLabel "Glob" Globtest.tests,
-                 TestLabel "MIMETypes" MIMETypestest.tests,
-                 TestLabel "Bitstest" Bitstest.tests,
-                 TestLabel "Eithertest" Eithertest.tests,
-                 TestLabel "ConfigParser.RunParser" ConfigParser.Parsertest.tests,
-                 TestLabel "ConfigParser.Main" ConfigParser.Maintest.tests,
-                 TestLabel "CRC32POSIXtest" CRC32POSIXtest.tests,
-                 TestLabel "CRC32GZIPtest" CRC32GZIPtest.tests,
-                 TestLabel "GZiptest" GZiptest.tests]
+tests = TestList [TestLabel "test1" test1]
 
 
