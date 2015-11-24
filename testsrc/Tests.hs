9c10ab8d3f4543aa35d4eea0a361d7b27b96c813
Axel Mannhardt <axel.mannhardt@freiheit.com>
2015-11-24 18:53:18 +0100

    added tests for priority order
    ... because I doubted the automatic Ord implementation to be what I expected, but everything is fine here.

--- testsrc/Tests.hs
+++ testsrc/Tests.hs
@@ -1,14 +1,22 @@
 {- arch-tag: Tests main file
 Copyright (C) 2004 John Goerzen <jgoerzen@complete.org>
 License: BSD3
-
 -}
-
 module Tests(tests) where
+
 import Test.HUnit
 
-test1 = TestCase ("x" @=? "x")
+import System.Log
 
-tests = TestList [TestLabel "test1" test1]
+tests = TestList [TestLabel "priority levels" priorityLevels]
 
+priorityLevels :: Test
+priorityLevels = TestList [
+  TestCase ( DEBUG <= DEBUG @=? True),
+  TestCase ( DEBUG <= INFO @=? True),
+  TestCase ( INFO <= WARNING @=? True),
+  TestCase ( WARNING <= ERROR @=? True),
+  TestCase ( INFO <= ERROR @=? True),
+  TestCase ( ERROR > INFO @=? True)
+  ]
 
